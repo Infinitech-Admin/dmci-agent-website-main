7528816500293b45a07fd97ebb6f50d2ebe4920a
InfinitechCorp <infinitechcorp.ph@gmail.com>
2025-05-14 16:40:24 +0800

    Update socmed.tsx

--- components/socmed.tsx
+++ components/socmed.tsx
@@ -1,56 +1,62 @@
 "use client";
-import { Image } from "@heroui/react";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 import React from "react";
-import { LuGlobe, LuMenu, LuX } from "react-icons/lu";
+import {
+  LuGlobe,
+  LuX,
+} from "react-icons/lu";
+import {
+  FaFacebookF,
+  FaPhoneAlt,
+  FaInstagram,
+  FaViber,
+  FaTelegramPlane,
+  FaWeixin,
+  FaWhatsapp,
+  FaEnvelope,
+} from "react-icons/fa";
 
-// Array of social media data
+// Array of social media data using icons
 const social = [
   {
     id: 1,
-    image:
-      "https://infinitech-testing5.online/media/facebook.png",
+    icon: <FaFacebookF className="text-white w-5 h-5" />,
     link: "https://www.facebook.com/share/We79XYCKWCDWmhE2/?mibextid=JRoKGi",
   },
   {
     id: 2,
-    image:
-      "https://infinitech-testing5.online/media/phone-call.png",
+    icon: <FaPhoneAlt className="text-white w-5 h-5" />,
     link: "tel:+639175480999",
   },
   {
     id: 3,
-    image:
-      "https://infinitech-testing5.online/media/mail.png",
+    icon: <FaEnvelope className="text-white w-5 h-5" />,
     link: "mailto:elladmcihomes.ph@gmail.com",
   },
   {
     id: 4,
-    image:
-      "https://infinitech-testing5.online/media/instagram.png",
+    icon: <FaInstagram className="text-white w-5 h-5" />,
     link: "https://www.instagram.com/ella.dmcihomes",
   },
   {
     id: 5,
-    image:
-      "https://infinitech-testing5.online/media/viber+(1).png",
+    icon: <FaViber className="text-white w-5 h-5" />,
     link: "viber://chat?number=639175480999",
   },
   {
     id: 6,
-    image:
-      "https://infinitech-testing5.online/media/telegram+(2).png",
+    icon: <FaTelegramPlane className="text-white w-5 h-5" />,
     link: "https://t.me/+639175480999",
   },
   {
     id: 7,
-    image: "/wechat.png",
+    icon: <FaWeixin className="text-white w-5 h-5" />,
     link: "weixin://dl/chat?number=639175480999",
   },
   {
     id: 8,
-    image: "/whatsapp.png",
+    icon: <FaWhatsapp className="text-white w-5 h-5" />,
     link: "https://wa.me/639175480999",
   },
 ];
@@ -68,20 +74,15 @@ const FloatingIcons = () => {
       {/* Desktop Social Icons */}
       <div className="fixed top-1/2 right-9 transform -translate-y-1/2 z-50 hidden lg:flex">
         <div className="flex flex-col gap-4">
-          {social.map((icon) => (
+          {social.map((item) => (
             <a
-              key={icon.id}
-              href={icon.link}
+              key={item.id}
+              href={item.link}
               rel="noopener noreferrer"
               target="_blank"
-              className="bg-blue-700 p-2 rounded-full shadow-lg hover:bg-blue-800 transition"
+              className="bg-blue-700 p-3 rounded-full shadow-lg hover:bg-blue-800 transition"
             >
-              <Image
-                alt={`Social icon ${icon.id}`}
-                height={32}
-                src={icon.image}
-                width={32}
-              />
+              {item.icon}
             </a>
           ))}
         </div>
@@ -103,20 +104,15 @@ const FloatingIcons = () => {
       {isOpen && (
         <div className="fixed bottom-28 right-8 flex flex-col items-end gap-2 lg:hidden z-[999] max-h-[70vh] overflow-y-auto">
           <div className="flex flex-col gap-2">
-            {social.map((icon) => (
+            {social.map((item) => (
               <a
-                key={icon.id}
-                href={icon.link}
+                key={item.id}
+                href={item.link}
                 rel="noopener noreferrer"
                 target="_blank"
-                className="bg-blue-700 p-2 rounded-full shadow-lg hover:bg-blue-800 transition"
+                className="bg-blue-700 p-3 rounded-full shadow-lg hover:bg-blue-800 transition"
               >
-                <Image
-                  alt={`Social icon ${icon.id}`}
-                  height={32}
-                  src={icon.image}
-                  width={32}
-                />
+                {item.icon}
               </a>
             ))}
           </div>
