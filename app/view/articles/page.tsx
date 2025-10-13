ff51214c99b9de80789dbd7e108ce8b4051305ed
InfinitechCorp <infinitechcorp.ph@gmail.com>
2025-10-13 08:47:28 +0800

    Update page.tsx

--- app/view/articles/page.tsx
+++ app/view/articles/page.tsx
@@ -26,7 +26,7 @@ const SingleNews = () => {
     const fetchNews = async () => {
       if (!id) return;
 
-      const apiUrl = process.env.NEXT_PUBLIC_API_URL || "https://infinitech-testing5.online";
+      const apiUrl = process.env.NEXT_PUBLIC_API_URL || "https://infinitech-api6.site";
       const headers = getAuthHeaders();
 
       try {
