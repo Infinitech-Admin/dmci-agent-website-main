a86b1a08b27ea026501e59f9f4be6a3c09d7f11c
InfinitechCorp <infinitechcorp.ph@gmail.com>
2025-10-13 08:31:46 +0800

    Update news.tsx

--- app/home/news.tsx
+++ app/home/news.tsx
@@ -27,7 +27,7 @@ const HomeNews = () => {
   useEffect(() => {
     const fetchNews = async () => {
       const apiUrl =
-        process.env.NEXT_PUBLIC_API_URL || "https://infinitech-testing5.online";
+        process.env.NEXT_PUBLIC_API_URL || "https://infinitech-api6.site";
       const headers = getAuthHeaders();
 
       try {
